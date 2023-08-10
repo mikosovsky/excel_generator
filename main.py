da56eef3884a5633c940664379eb2da5c60efb99
mikosovsky <mikolaj.dawczyk@gmail.com>
2023-08-10 18:03:44 +0200

    Added handle errors

--- main.py
+++ main.py
@@ -79,23 +79,3 @@ while True:
                 sg.popup_error("Choose right date!")
         else:
             sg.popup_error("Choose the file or fill up textbox with content!")
-
-# # Requesting for month and year
-#
-# month = int(input("Podaj miesiąc: "))
-# year = int(input("Podaj rok: "))
-#
-#
-# excel_generator.change_month(month, year)
-#
-# # Ask for quantity of people in excel
-# people_quantity = int(input("Podaj ilość osób do dodania w excelu: "))
-# people = []
-#
-# # Requesting for people and adding them to list
-# for i in range(people_quantity):
-#     person = input(f"Podaj dane {i+1}. osoby: ")
-#     people.append(person)
-#
-# excel_generator.change_people_list(people)
-# excel_generator.generate_excel()
